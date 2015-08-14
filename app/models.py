a891cd93158f9aa27d46e82b30339ef4f86ff896
jkerr <jkerr@zenoss.com>
2015-08-13 22:26:46 -0500

    added 'models' file with User model, MongoEngine db init, etc.

---
+++ app/models.py
@@ -0,0 +1,26 @@
+__author__ = 'jkerr'
+from flask_mongoengine import MongoEngine
+from flask_security import RoleMixin, UserMixin
+
+
+
+
+db = MongoEngine()
+
+
+
+
+
+class Role(db.Document, RoleMixin):
+    name = db.StringField(max_length=80, unique=True)
+    description = db.StringField(max_length=255)
+
+class User(db.Document, UserMixin):
+    email = db.StringField(max_length=255)
+    password = db.StringField(max_length=255)
+    active = db.BooleanField(default=True)
+    confirmed_at = db.DateTimeField()
+    roles = db.ListField(db.ReferenceField(Role), default=[])
+
+
+
